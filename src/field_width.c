8954cd88044737b69bb0fb7d6f397f9bf0dfad21
Victor Jung <vic.jung@live.fr>
2015-01-27 03:09:59 +0100

    precision

--- src/field_width.c
+++ src/field_width.c
@@ -1,11 +1,12 @@
-void			find_field_width(char *str, t_info *info)
+void			find_field_width(const char *str, t_info *info)
 {
 	char			*field_width;
 
-	while (str[LEFT_I] && ft_isdigit(str[LEFT_I]))
-		LEFT_I++;
-	field_width = (char *)malloc(sizeof(char) * (LEFT_I - RIGHT_I));
-	field_width = ft_strncpy(field_width, str + RIGHT_I, LEFT_I - RIGHT_I);
-	RIGHT_I = LEFT_I;
+	while (str[RIGHT_I] && ft_isdigit(str[RIGHT_I]))
+		RIGHT_I++;
+	field_width = (char *)malloc(sizeof(char) * (RIGHT_I - LEFT_I + 1));
+	field_width = ft_strncpy(field_width, str + LEFT_I, RIGHT_I - LEFT_I);
+	LEFT_I = RIGHT_I;
 	info->width = ft_atoi(field_width);
+	free (field_width);
 }
---
+++ src/precision.c
@@ -0,0 +1,16 @@
+void				find_precision(const char *str, t_info *info)
+{
+	char				*precision;
+
+	if (str[RIGHT_I] && str[RIGHT_I] == '.')
+	{
+		RIGHT_I++;
+		while (str[RIGHT_I] && ft_isdigit(str[RIGHT_I]))
+			RIGHT_I++;
+		precision = (char *)malloc(sizeof(char) * (RIGHT_I - LEFT_I));
+		precision = ft_strncpy(precision, str + LEFT_I + 1, RIGHT_I - LEFT_I);
+		LEFT_I = RIGHT_I;
+		info->precision = ft_atoi(precision);
+		free (precision);
+	}
+}
