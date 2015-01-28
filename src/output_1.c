a53d2e115708a57dc803c6eb037b1fb2e099f2c5
cnev <orthana103@gmail.com>
2015-01-28 23:15:37 +0100

    output

--- src/output_1.c
+++ src/output_1.c
@@ -9,9 +9,25 @@ char			*build_s(void)
 	return (ft_strdup(va_arg(VARS, char *)));
 }
 
+/* tmp to move to libft or anywhere */
+size_t			*ft_wcstrlen(wchar_t *str)
+{
+	int				i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+wchar_t			*ft_wcstrdup(wchar_t *str)
+{
+
+}
+
 char			*build_maj_s(void)
 {
-	int				val;
+	wchar_t			*val;
 
-	val = va_arg(VARS, int);
+	val = va_arg(VARS, wchar_t *);
 }
