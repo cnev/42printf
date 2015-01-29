bd259611c77e6cf346f1f41e26a5b1d0ba6c1a1d
Victor Jung <vic.jung@live.fr>
2015-01-29 02:07:25 +0100

    data

---
+++ src/data.c
@@ -0,0 +1,22 @@
+#include "../includes/ft_printf.h"
+
+t_data				*create_data(char *str, wchar_t *w_str)
+{
+	t_data				data;
+
+	if (!str && w_str)
+	{
+		data.is_wchar = TRUE;
+		data.str = NULL;
+		data.w_str = w_str;
+	}
+	else if (!w_str && str)
+	{
+		data.is_wchar = FALSE;
+		data.str = str;
+		data.w_str = NULL;
+	}
+	else
+		return (NULL);
+	return (&data);
+}
